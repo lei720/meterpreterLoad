1b1225008b640252c802e72bdd77bb771ea4c4db
lei720 <lei_7420042@163.com>
2015-02-16 19:40:32 +0800

    source code

---
+++ build.bat
@@ -0,0 +1,2 @@
+set PATH=%PATH%;d:\mingw\bin
+gcc -L d:\mingw\lib src\main.c -o loader.exe -lws2_32
---
+++ build2.bat
@@ -0,0 +1,2 @@
+set PATH=%PATH%;d:\mingw\bin
+gcc -L d:\mingw\lib src\main2.c -o loader.exe -lws2_32
